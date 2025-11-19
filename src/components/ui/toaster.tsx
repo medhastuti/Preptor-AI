f425bb130cf75d4a0d7e08f1e51faf4d7cdcd6ab
Medha <medhastuti@gmail.com>
2025-11-20 02:23:57 +0530

    Update toaster.tsx
    Refactored toast rendering to use arrow function syntax for cleaner code.

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -6,19 +6,25 @@ export function Toaster() {
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
-        return (
-          <Toast key={id} {...props}>
-            <div className="grid gap-1">
-              {title && <ToastTitle>{title}</ToastTitle>}
-              {description && <ToastDescription>{description}</ToastDescription>}
-            </div>
-            {action}
-            <ToastClose />
-          </Toast>
-        );
-      })}
-      <ToastViewport />
+      {toasts.map(({ id, title, description, action, ...props }) => (
+        <Toast key={id} {...props}>
+          <div className="grid gap-1">
+            {title && <ToastTitle>{title}</ToastTitle>}
+            {description && <ToastDescription>{description}</ToastDescription>}
+          </div>
+          {action}
+          <ToastClose />
+        </Toast>
+      ))}
+
+      {/* Bottom-right on ALL devices */}
+      <ToastViewport
+        className="
+          fixed 
+          bottom-6 right-6 
+          w-full max-w-sm
+        "
+      />
     </ToastProvider>
   );
 }
